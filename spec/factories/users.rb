3a16c8e64ba012e7217a5e0c4cbe9afeaed39352
Ethan Streadbeck <estreadbeck@gmail.com>
2018-02-21 14:50:58 -0700

    Added Factory Firl for Posts and User tests

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,20 @@
 FactoryGirl.define do 
+  sequence :email do |n|
+    "test#{n}@test.com"
+  end
+
   factory :user do
     first_name 'Jon'
     last_name 'Snow'
-    email "test@test.com"
+    email { generate :email }
+    password 'qwerty'
+    password_confirmation "qwerty"
+  end
+
+  factory :second_user, class: "User" do
+    first_name 'Robb'
+    last_name 'Stark'
+    email { generate :email }
     password 'qwerty'
     password_confirmation "qwerty"
   end
@@ -10,7 +22,7 @@ FactoryGirl.define do
   factory :admin_user, class: "AdminUser" do
     first_name "Admin"
     last_name "User"
-    email "admin@user.com"
+    email { generate :email }
     password "ytrewq"
     password_confirmation "ytrewq"
   end
