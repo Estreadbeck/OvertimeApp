11c93ad2f74d0b93df9c5b2290ec9a68b2fc3014
Ethan Streadbeck <estreadbeck@gmail.com>
2018-02-21 16:16:37 -0700

    Added edit function for posts

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,12 @@
+
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
   before do
     @user = FactoryGirl.create(:user)
   end
-  describe "Creation" do
-      
+
+  describe "creation" do
     it "can be created" do
       expect(@user).to be_valid
     end
