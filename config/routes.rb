2277cd677c0d530b718e74d9e7fd44a11e43db63
Ethan Streadbeck <estreadbeck@gmail.com>
2018-03-07 09:36:53 -0700

    added styles for audits and enums for status

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   resources :audit_logs, except: [:new, :edit, :destroy]
-
-  resources :audit_logs
   namespace :admin do
     resources :users
     resources :posts
