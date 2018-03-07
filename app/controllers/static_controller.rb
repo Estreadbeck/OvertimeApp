1a9a101ea65ddb094290821a43bf40388bd68d7d
Ethan Streadbeck <estreadbeck@gmail.com>
2018-03-07 14:10:23 -0700

    finished admin homepage style and pagination, started employee homepage changes

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,5 +1,10 @@
 class StaticController < ApplicationController
   def homepage
-    @pending_approvals = Post.where(status: 'submitted')
+    if admin_types.include?(current_user.type)
+      @pending_approvals = Post.where(status: 'submitted').page(params[:page]).per(9)
+      @recent_audit_items = AuditLog.last(9)
+    else
+      #something else
+    end
   end
 end
