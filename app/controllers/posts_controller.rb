60d9f81f59ae598d83c295db85a02ee648436ac0
Ethan Streadbeck <estreadbeck@gmail.com>
2018-03-07 11:49:39 -0700

    added button functionality and approval for admins

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :set_post, only: [:show, :edit, :update, :destroy, :approve]
   
   def index
     @posts = Post.posts_by(current_user).page(params[:page]).per(10)
@@ -33,6 +33,12 @@ class PostsController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def approve
+    authorize @post
+    @post.approved!
+    redirect_to root_path, notice: "The post has been approved"
   end
 
   def show
