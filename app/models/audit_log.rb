d9cf856c3c1f72c754fd7dc030cb7ec20af0f511
Ethan Streadbeck <estreadbeck@gmail.com>
2018-03-08 14:27:50 -0700

    Attempted to fix rspec bug, but gave up instead

--- app/models/audit_log.rb
+++ app/models/audit_log.rb
@@ -4,10 +4,15 @@ class AuditLog < ApplicationRecord
   validates_presence_of :user_id, :status, :start_date
   after_initialize :set_defaults
 
+ 
+
   private
 
   def set_defaults
     self.start_date ||= Date.today - 6.days
   end
-    
+   
+  
+
+
 end
