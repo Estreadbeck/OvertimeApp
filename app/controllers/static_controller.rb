953ebb6a52fb9ce70c3d267752183b0b1bce4651
Ethan Streadbeck <estreadbeck@gmail.com>
2018-07-20 17:25:15 -0600

    updated ruby gem versions

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -4,7 +4,7 @@ class StaticController < ApplicationController
       @pending_approvals = Post.where(status: 'submitted').page(params[:page]).per(9)
       @recent_audit_items = AuditLog.last(9)
     else
-      @pending_audit_confirmations = current_user.audit_logs.pending
+      @pending_audit_confirmations = current_user.audit_logs.pending.by_start_date
     end
   end
 end
